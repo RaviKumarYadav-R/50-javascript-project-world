3ad97209a6f89288fb2c7ebc8f1a46a880b2f683
Infinite Css <technosmartravioffical@gmail.com>
2025-07-09 09:14:32 +0000

    followers card

--- basic js projects/test.js
+++ basic js projects/test.js
@@ -1,1 +1,18 @@
-console.log("ravi")
+let avtar = document.querySelector("#avtar")
+let follower = document.querySelector(".follower-count")
+
+
+const apiUrl = "https://api.github.com/users/hiteshchoudhary";
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", apiUrl);
+xhr.onreadystatechange = function() {
+  console.log(xhr.readyState);
+  if (xhr.readyState === 4) {
+    const data = JSON.parse(this.responseText);
+    avtar.src = data.avatar_url;
+    follower.innerHTML = `${data.followers} followers`
+    
+  }
+};
+xhr.send();
