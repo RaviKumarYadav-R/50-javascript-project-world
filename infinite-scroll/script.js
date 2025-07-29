1a9350b1ff26620b20263ab12d09e88293137298
Ravi Kumar <technosmartravioffical@gmail.com>
2025-07-29 12:34:24 +0530

    infinite scroll

---
+++ infinite-scroll/script.js
@@ -0,0 +1,135 @@
+const loremText = ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laboriosam
+        vitae, aperiam laudantium ad porro expedita rem placeat id officia
+        nesciunt ipsa eaque est sint itaque ducimus ipsum dolor iusto. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Officia aliquam
+        vitae officiis rerum amet. Laborum quibusdam eligendi rem consectetur
+        excepturi expedita magnam eius odio minima suscipit. Aut ipsam
+        temporibus maxime. Numquam porro exercitationem earum doloremque neque
+        autem dolore expedita consequatur, a consectetur iste odio magnam
+        mollitia voluptatem libero, quibusdam voluptatibus placeat reiciendis
+        unde ratione, nisi adipisci! Iste fugiat aperiam eum. Optio et ad,
+        temporibus nihil quod culpa, inventore harum eos nisi provident
+        aspernatur quidem porro error in accusamus est impedit dignissimos
+        possimus sequi cumque ipsum corporis eligendi. Tenetur, beatae sed.
+        Consequuntur dolore ipsa in assumenda fugiat vero autem commodi, illum
+        quia. Natus laudantium sint quas perferendis dicta saepe ad repellendus
+        porro eveniet qui. Voluptates perspiciatis provident voluptate.
+        Accusantium, provident est. Illum voluptatum magnam itaque facilis quae
+        nisi totam deleniti blanditiis voluptatibus amet obcaecati dolore
+        aspernatur nihil doloremque, numquam quos sint laudantium aut optio
+        consectetur! Cupiditate aspernatur ad fuga fugiat itaque? Dolore ducimus
+        qui unde laboriosam minima, iusto dicta quas illo repudiandae
+        praesentium repellendus autem, blanditiis perspiciatis ea corrupti
+        dolorem maxime consectetur nemo sapiente? Praesentium a voluptate,
+        corrupti architecto at aliquam? Voluptate perspiciatis, totam, cumque
+        adipisci aspernatur harum ratione ipsa ex impedit, nobis aut quaerat
+        assumenda quis officiis cum quisquam possimus quae rem quam non nostrum
+        doloremque tempora sint? Soluta, doloremque! Laborum et illum eos quia a
+        non, itaque vero maxime impedit explicabo veritatis, dolorum eveniet,
+        molestiae obcaecati earum reiciendis ab dolor doloribus sunt aliquam
+        repudiandae? Aliquid tempora dicta quasi necessitatibus? Molestias
+        tempora beatae magnam? Quae non voluptatem unde, perspiciatis dolorum
+        itaque illo perferendis repudiandae excepturi? Nemo enim tempora
+        architecto modi aperiam, harum minus quae laborum eligendi molestiae ea
+        dolorem esse! Alias perferendis eos debitis ipsam, inventore suscipit
+        pariatur atque officiis optio adipisci quo ipsum quia eligendi explicabo
+        vitae aliquid sequi facere ea ullam? Cumque minus vel eligendi. Rerum,
+        quae quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Expedita eos molestiae nulla rerum incidunt dicta veritatis, nam hic
+        harum adipisci nobis ut non voluptates similique provident asperiores
+        magni fugit accusamus! Dolor ab officia, accusamus rem, fugiat at
+        sapiente ipsa molestiae facere ducimus aliquam unde et quas. Officiis
+        maiores, atque earum accusamus incidunt voluptas alias amet ipsam beatae
+        deleniti id eligendi. Omnis autem, consectetur aperiam optio minima odit
+        minus laudantium voluptatum quis assumenda. Praesentium, veritatis!
+        Totam excepturi maiores atque nulla omnis ratione porro fugit cumque
+        deserunt rem. Eos facilis quos id? Accusamus omnis dolorum doloremque,
+        molestias illo sunt, quo quasi eius adipisci reiciendis provident
+        placeat labore rem necessitatibus blanditiis, doloribus qui! Ab culpa
+        ipsa, assumenda natus necessitatibus dignissimos officia velit facere!
+        Repudiandae doloribus minus dolore. Fugiat asperiores earum veritatis
+        esse inventore, id doloribus saepe dolor iste voluptatum quidem
+        repellendus hic! Officiis odio asperiores eius maxime magni similique
+        voluptate dignissimos eveniet. Nulla. Similique quas tempore consequatur
+        unde vero obcaecati, assumenda laboriosam officiis nulla possimus
+        voluptatum dolor minima ullam quos explicabo quibusdam itaque dicta
+        labore saepe, nisi ab. Aspernatur voluptatibus possimus perferendis
+        consequatur. Voluptatem neque perferendis vel, nihil fugiat cumque est
+        rem enim vitae placeat quas aliquam praesentium accusamus aliquid saepe
+        dicta similique cupiditate accusantium iste eius nesciunt doloribus! Et
+        dolor reprehenderit culpa? Officia nostrum, accusantium, praesentium
+        dolores, ab maxime exercitationem tempore tenetur minus eius esse
+        laudantium possimus impedit repellendus quisquam rerum cumque rem. Nihil
+        enim cumque quae ullam consequuntur, repellat tempora facere. Delectus
+        dicta eaque officia ratione illo eveniet, exercitationem perferendis
+        deleniti dolorum! Aperiam soluta blanditiis autem ipsa et repellendus
+        assumenda reiciendis maxime minima, numquam sit vitae accusantium velit
+        est voluptas deleniti! Veritatis fugit, cupiditate, nam reiciendis optio
+        vero, assumenda pariatur doloribus distinctio eligendi eius debitis?
+        Voluptatum ipsam ea molestias nostrum nemo at vero consectetur maiores
+        fuga quibusdam soluta, magni quos atque! Ipsum ullam, aliquam obcaecati
+        repellendus, numquam cumque facere vel mollitia dolorum sit fuga. Veniam
+        necessitatibus earum doloribus eum quaerat ipsa atque saepe animi odio
+        eaque voluptates, explicabo minus voluptatum mollitia. Dolorem
+        laboriosam exercitationem corrupti dolorum voluptate numquam rem. Porro
+        suscipit saepe, laborum, ut ipsam illo eveniet reiciendis accusamus vel
+        quas minus assumenda, ad deleniti unde recusandae quisquam sed adipisci
+        repudiandae? Tenetur, commodi. Neque iusto nam eligendi nisi, eos
+        expedita, totam quae earum nostrum, sed voluptatum? Debitis vel fugiat
+        ad architecto quas saepe? Est sit voluptatem aperiam enim autem aliquam
+        deleniti? Recusandae exercitationem commodi laboriosam quae architecto
+        repellat asperiores quia nihil veniam a libero et ipsa delectus labore
+        reiciendis saepe, in cum? Saepe quaerat nemo repudiandae consequatur quo
+        placeat, animi deserunt! Odit rem veniam aut, quidem dolorum qui ea!
+        Reprehenderit, cupiditate! Fuga exercitationem ex maxime veritatis.
+        Voluptatem explicabo blanditiis vero dolorum eos reiciendis ducimus
+        consequuntur atque dignissimos repudiandae, quibusdam non expedita!
+        Explicabo perspiciatis officiis, necessitatibus, aspernatur eligendi
+        optio soluta repellendus tenetur ratione modi blanditiis nostrum
+        corporis temporibus, nesciunt unde cumque. Non necessitatibus nihil nemo
+        ratione quae voluptas, explicabo cum repellendus architecto. Incidunt
+        tenetur, aspernatur nulla impedit quibusdam nesciunt. Quasi molestiae
+        quidem porro mollitia cumque laudantium amet eligendi veniam totam
+        officiis praesentium adipisci magni quisquam, placeat delectus aperiam
+        vitae, voluptate ex dolore! Ab commodi, molestiae sunt ipsam, labore
+        doloribus et, voluptatibus velit nam magni voluptates dolore? Architecto
+        ad tempora minus exercitationem fugiat illo facere eaque, aliquid, hic
+        qui quas, asperiores quod cupiditate! Quidem necessitatibus sed omnis
+        non dolorum dignissimos blanditiis quam odio itaque mollitia repellat
+        praesentium assumenda, vel, qui, iusto reiciendis libero eos! Dolorem
+        alias iste sit. Quas consequatur veniam fugiat error. Veritatis aperiam
+        voluptatem nisi laboriosam ut vel officia at ducimus aut ab obcaecati
+        odit veniam dicta, impedit rerum excepturi? Quae nemo minus dolorem
+        corrupti eligendi sunt maiores quia ex molestias!`;
+
+let page = 1;
+const loading = document.querySelector(".loading");
+function generateContent() {
+  const text = loremText.split(". ");
+  const lorem = text.slice(
+    Math.floor(Math.random() * (text.length - 3)),
+    text.length
+  );
+
+  loading.classList.remove("hidden");
+  setTimeout(() => {
+    loading.classList.add("hidden");
+    page++;
+    const div = document.createElement("div");
+    div.classList.add("content-data");
+    div.innerHTML = `<h2>Article ${page}</h2>
+  <p>${lorem.join(". ")}</p>`;
+    document.getElementById("content").appendChild(div);
+  }, Math.random() * 3 * 1000);
+}
+
+window.addEventListener("scroll", () => {
+  if (
+    document.documentElement.scrollTop +
+      document.documentElement.clientHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    generateContent();
+    console.log("ok");
+  }
+});
