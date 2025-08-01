53a42832039bd4de8f210f639eb78d3b8a6a4e07
Ravi Kumar <technosmartravioffical@gmail.com>
2025-08-01 08:12:18 +0530

    least more projects

--- script.js
+++ script.js
@@ -124,6 +124,131 @@ const projects = [
     name: "typing-speed-test",
     imageLink: "25-typing-speed-test.png",
   },
+  {
+    number: 26,
+    name: "rps-game",
+    imageLink: "26-rps-game.png",
+  },
+  {
+    number: 27,
+    name: "pomodoro-timer",
+    imageLink: "27-pomodoro-timer.png",
+  },
+  {
+    number: 28,
+    name: "expense-tracker",
+    imageLink: "28-expense-tracker.png",
+  },
+  {
+    number: 29,
+    name: "number-guessing",
+    imageLink: "29-number-guessing.png",
+  },
+  {
+    number: 30,
+    name: "accordion",
+    imageLink: "30-accordion.png",
+  },
+  {
+    number: 31,
+    name: "tabs-component",
+    imageLink: "31-tabs-component.png",
+  },
+  {
+    number: 32,
+    name: "scroll-progress",
+    imageLink: "32-scroll-progress.png",
+  },
+  // {
+  //   number: 33,
+  //   name: "copy-to-clipboard",
+  //   imageLink: "33-copy-to-clipboard.png",
+  // },
+  // {
+  //   number: 34,
+  //   name: "char-counter",
+  //   imageLink: "34-char-counter.png",
+  // },
+  // {
+  //   number: 35,
+  //   name: "star-rating",
+  //   imageLink: "35-star-rating.png",
+  // },
+  // {
+  //   number: 36,
+  //   name: "random-user",
+  //   imageLink: "36-random-user.png",
+  // },
+  // {
+  //   number: 37,
+  //   name: "drag-drop",
+  //   imageLink: "37-drag-drop.png",
+  // },
+  // {
+  //   number: 38,
+  //   name: "stopwatch",
+  //   imageLink: "38-stopwatch.png",
+  // },
+  // {
+  //   number: 39,
+  //   name: "file-upload",
+  //   imageLink: "39-file-upload.png",
+  // },
+  // {
+  //   number: 40,
+  //   name: "infinite-scroll",
+  //   imageLink: "40-infinite-scroll.png",
+  // },
+  // {
+  //   number: 41,
+  //   name: "sticky-notes",
+  //   imageLink: "41-sticky-notes.png",
+  // },
+  // {
+  //   number: 42,
+  //   name: "qr-generator",
+  //   imageLink: "42-qr-generator.png",
+  // },
+  // {
+  //   number: 43,
+  //   name: "file-drag-and-drop",
+  //   imageLink: "43-file-drag-and-drop.png",
+  // },
+  // {
+  //   number: 44,
+  //   name: "drawing-app",
+  //   imageLink: "44-drawing-app.png",
+  // },
+  // {
+  //   number: 45,
+  //   name: "login-ui",
+  //   imageLink: "45-login-ui.png",
+  // },
+  // {
+  //   number: 46,
+  //   name: "response-navbar",
+  //   imageLink: "46-response-navbar.png",
+  // },
+  // {
+  //   number: 47,
+  //   name: "github-finder",
+  //   imageLink: "47-github-finder`.png",
+  // },
+  // {
+  //   number: 48,
+  //   name: "joke-generator",
+  //   imageLink: "48-joke-generator.png",
+  // },
+  // {
+  //   number: 49,
+  //   name: "image-search",
+  //   imageLink: "49-image-search.png",
+  // },
+  // {
+  //   number: 50,
+  //   name: "portfolio",
+  //   imageLink: "50-portfolio.png",
+  // },
 ];
 
 const projectSection = document.querySelector(".project-section");
